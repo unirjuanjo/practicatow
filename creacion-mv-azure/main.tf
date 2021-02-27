a58c11b90e837e9f545ea49b23f9d953d37a5acf
unirjuanjo <77353697+unirjuanjo@users.noreply.github.com>
2021-02-27 21:36:09 +0100

    Update main.tf

--- creacion-mv-azure/main.tf
+++ creacion-mv-azure/main.tf
@@ -11,10 +11,10 @@ terraform {
   }
 }
 provider "azurerm" {  
-  client_id       = "${var.azure_client_id}"
-  client_secret   = "${var.azure_client_secret}"
-  subscription_id = "${var.azure_subscription_id}"
-  tenant_id       = "${var.azure_tenant_id}"
+  client_id       = "${var.azure_client_id}"          #  461de720-75d4-4667-b60c-9b31e7432cba
+  client_secret   = "${var.azure_client_secret}"      #  RthD_5m_BiJICZCCkTm1PBUoL2m9JvH6t3
+  subscription_id = "${var.azure_subscription_id}"    #  88676003-704a-4be1-a001-d538140f9087
+  tenant_id       = "${var.azure_tenant_id}"          #  899789dc-202f-44b4-8472-a6d40f9eb440
 }
 
 #################  RESOURCE GROUP  ####################################
@@ -33,8 +33,8 @@ resource "azurerm_resource_group" "resource_group" {
 resource "azurerm_virtual_network" "virtual_network" {
     name                = "unir_virtual_network"
     address_space       = ["10.0.0.0/16"]
-    location            = azure_resource_group.resource_group.location
-    resource_group_name = azure_resource_group.resource_group.name
+    location            = azurerm_resource_group.resource_group.location
+    resource_group_name = azurerm_resource_group.resource_group.name
 
     tags = {
         environment = "unir_practica_2"
@@ -53,11 +53,12 @@ resource "azurerm_subnet" "subnet" {
 }
 
 
-########################## GRUPO DE SEGURIDAD DE RED #########################################
+########################## GRUPO DE SEGURIDAD DE RED x 2#########################################
 # Creación de un grupo de seguridad de red
 resource "azurerm_network_security_group" "network_security_group" {
-    name                = "unir_network_security_group"
-    location            = azure_resource_group.resource_group.location
+   count                = var.num_mv_instancias
+   name                 = "unir_network_security_group${count.index}"
+    location            = azurerm_resource_group.resource_group.location
     resource_group_name = azurerm_resource_group.resource_group.name
 
     security_rule {
@@ -79,39 +80,51 @@ resource "azurerm_network_security_group" "network_security_group" {
 
 
 # Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "network_interface_security_group_association" {
-    network_interface_id      = azurerm_network_interface.network_interface.id
-    network_security_group_id = azurerm_network_security_group.network_security_group.id
+#resource "azurerm_network_interface_security_group_association" "network_interface_security_group_association" {
+#    network_interface_id      = azurerm_network_interface.network_interface.id
+#    network_security_group_id = azurerm_network_security_group.network_security_group.id
   
+resource "azurerm_network_interface_security_group_association" "network_interface_security_group_association" {
+#    network_interface_id      = azurerm_network_interface.network_interface.id
+#    network_security_group_id = azurerm_network_security_group.network_security_group.id
+    network_interface_id      = "${element(azurerm_network_interface.network_interface.*.id,count.index)}"
+    network_security_group_id = "${element(azurerm_network_security_group.network_security_group.*.id,count.index)}"
 
-######################## IP PUBLICA ###################################
+######################## IP PUBLICA x 2 ###################################
 
 # Creación de una dirección IP pública
 resource "azurerm_public_ip" "public_ip" {
-    name                         = "unir_public_ip"
-    location                     = azure_resource_group.resource_group.location
+   count                         = var.num_mv_instancias
+   name                          = "unir_public_ip${count.index}"
+    location                     = azurerm_resource_group.resource_group.location
     resource_group_name          = azurerm_resource_group.resource_group.name
     allocation_method            = "Dynamic"
+    sku                          = "Basic"
 
    tags = {
         environment = "unir_practica_2"
    }
 }
   
-############################# TARJETA DE RED ######################################
+############################# TARJETA DE RED x 2 ######################################
 # Creación de una tarjeta de interfaz de red virtual
 resource "azurerm_network_interface" "network_interface" {
-    count                       = "${var.vm}"
-    name                        = "unir_network_interface"
-    location                    = azure_resource_group.rg.location
-    resource_group_name         = azure_resource_group.rg.name
+    count                       =  var.num_mv_instancias
+    name                        = "unir_network_interface_${count.index}"
+    location                    = azurerm_resource_group.resource_group.location
+    resource_group_name         = azurerm_resource_group.resource_group.name
+
 
     ip_configuration {
-        name                          = "myNicConfiguration_01"
+        name                          = "ip_$count.index}"
         subnet_id                     = azurerm_subnet.myterraformsubnet.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
-    }
+        #private_ip_address_allocation = "Dynamic"
+        private_ip_address_allocation = "Static"
+        #private_ip_address            = "10.0.1.11"
+        private_ip_address            = "10.0.1.1${count.index}"
+        #public_ip_address_id          = azurerm_public_ip.public_ip.id
+        public_ip_address_id          = "${element(azurerm_public_ip.public_ip.*.id, count.index)}"
+   }
 
     tags = {
         environment = "unir_practica_2"
@@ -119,7 +132,7 @@ resource "azurerm_network_interface" "network_interface" {
 }
 
 
-####################### CRECION DE MAQUINAS VIRTUALES #############################
+####################### CRECION DE MAQUINAS VIRTUALES x 2 #############################
 # El último paso es crear una máquina virtual y usar todos los recursos creados. En la siguiente sección se crea una máquina virtual llamada myVM 
 # y se asocia una NIC virtual llamada myNIC. Se usa la imagen Ubuntu 18.04-LTS más reciente y se crea un usuario llamado azureuser con la autenticación
 # de contraseña deshabilitada.
@@ -130,22 +143,54 @@ resource "tls_private_key" "example_ssh" {
 }
 
 output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
+   
+   
 
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    size                  = "Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "linux_virtual_machine" {
+    count                           =  var.num_mv_instancias
+    name                            = "unir_linux_virtual_machine_${count.index}" 
+    location                        = azurerm_resource_group.resource_group.location
+    resource_group_name             = azurerm_resource_group.resource_group.name
+    network_interface_ids           = ["${element(azurerm_network_interface.network_interface.*.id, count.index)}"]
+    disable_password_authentication = true
+   
+    admin_ssh_key {
+       username      = "adminUser"
+       public_key    = file("C:\# AA UNIR\VanDyke\Identity.pub")
+   }
+   
+   os_disk {
+      caching  = "ReadWirite"
+      storage_account_type = "Stadard_LRS"
+   }
+   
+   plan {
+      name = "centos-8-steeam-free"
+      product = "centos-8-steeam-free"
+      publiher = "cognosys"
+      
+   source_image_reference {
+      publiher = "cognosys"
+      offfer   = "centos-8-steeam-free"
+      sku      = "centos-8-steeam-free"
+      version  = "1.2019.0810"
+   }
+   
+   storage_os_disk {
+    name          = "demo-disk-${count.index}"
+    vhd_uri       = "${azurerm_storage_account.demo.primary_blob_endpoint}${element(azurerm_storage_container.demo.*.name, count.index)}/demo.vhd"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
 
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-
-#####################################################################################
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
+      
+   boot_diagnostics {
+      storage_account_uri  = azurem_torage_account.stAccount.primary_blob_endpoint
+      
+      
+  #####################################################################################
 
 # Creación de una cuenta de almacenamiento para el diagnóstico
 
@@ -175,6 +220,18 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
 #         environment = "Terraform Demo"
 #     }
 # }
+      
+resource "azurerm_storage_account" "storage_account" {
+     name                        = "unir_storage_account"
+     resource_group_name         = azurerm_resource_group.resource_group.name
+     location                    = azurerm_resource_group.resource_group.location
+     account_replication_type    = "LRS"
+     account_tier                = "Standard"
+
+     tags = {
+        environment = "unir_practica_2"
+    }
+ }
 
 #####################################################################################
 # El último paso es crear una máquina virtual y usar todos los recursos creados. En la siguiente sección se crea una máquina virtual llamada myVM 
@@ -218,10 +275,10 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     }
 
     boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+        storage_account_uri = azurerm_storage_account.storage_account.primary_blob_endpoint
     }
 
-    tags = {
-        environment = "Terraform Demo"
+   tags = {
+        environment = "unir_practica_2"
     }
 }
